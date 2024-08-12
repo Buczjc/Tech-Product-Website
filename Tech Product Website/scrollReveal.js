cfaa09c5997c351bbac1513ced66316d0553a760
bucz <119478207+Buczjc@users.noreply.github.com>
2024-08-12 23:07:23 +0800

    Add files via upload

---
+++ Tech Product Website/scrollReveal.js
@@ -0,0 +1,81 @@
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+ScrollReveal().reveal(".header", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".first_content_container", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".first_content_img", {
+  delay: 300,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".first_content_second_page_container", {
+  delay: 100,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".second_content-img", {
+  delay: 300,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".third_content h1", {
+  delay: 100,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".shop_wrapper", {
+  delay: 200,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".third_content button", {
+  delay: 300,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".expert_support", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".out_locations", {
+  delay: 300,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".fourt_content_img_block", {
+  delay: 100,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".contact-header", {
+  delay: 100,
+  origin: "top",
+});
+
+ScrollReveal().reveal(".contact-info", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".social-media", {
+  delay: 100,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".email", {
+  delay: 200,
+  origin: "top",
+});
